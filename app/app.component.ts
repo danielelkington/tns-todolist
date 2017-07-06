4b11a5191051c5f73dedd0ffbbc2ce27734301d1
Daniel Elkington <danielelkington@me.com>
2017-07-06 16:54:31 +1000

    Adding SQLite to persist data to local DB

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { ListItem } from "./listItem";
 import dialogs = require("ui/dialogs");
+var Sqlite = require("nativescript-sqlite");
+var Toolbox = require("nativescript-toolbox");
 
 @Component({
     selector: "ns-app",
@@ -8,12 +10,39 @@ import dialogs = require("ui/dialogs");
 })
 export class AppComponent { 
     listItems: Array<ListItem>;
+    database: any;
+
+    private initialiseDatabaseIfNotExists(){
+        //Initialise database if it doesn't exist
+        (new Sqlite("todolist.db")).then(db => {
+            db.execSQL("CREATE TABLE IF NOT EXISTS ListItem " +
+                    "(Id TEXT PRIMARY KEY, Description TEXT, Complete INTEGER NOT NULL, ModifiedUTC INTEGER)")
+            .then(id => {
+                this.database = db;
+                this.fetchItems();
+            }, error => {
+                console.log("CREATE TABLE ERROR", error);
+            });
+        }, error => {
+            console.log("OPEN DB ERROR", error);
+        });
+    }
 
     constructor(){
-        this.listItems = [
-            new ListItem("Do Homework"),
-            new ListItem("Watch Television", true)
-        ];
+        this.initialiseDatabaseIfNotExists();
+    }
+
+    private fetchItems(){
+        this.listItems = [];
+        var listItems = this.listItems;
+        var itemCheckChanged = this.itemCheckChanged;
+        this.database.each("SELECT Id, Description, Complete FROM ListItem", function (err, row) {
+            if (err){
+                console.log("SELECT ERROR", err);
+                return;
+            }
+            listItems.push(new ListItem(row[0], row[1], row[2]));
+        });
     }
 
     addItem(){
@@ -25,7 +54,25 @@ export class AppComponent {
             if (!r.result || !r.text){
                 return;
             }
-            this.listItems.push(new ListItem(r.text));
-        })
+            var newItem = new ListItem(Toolbox.uuid(), r.text);
+            this.database.execSQL("INSERT INTO ListItem(Id, Description, Complete, ModifiedUTC) VALUES(?,?,?,?)",
+                [newItem.id, newItem.description, 
+                newItem.complete ? 1 : 0, (new Date()).getUTCMilliseconds()]).then(id => {
+                }, error =>{
+                    console.log("INSERT ERROR", error);
+                });
+            this.listItems.push(newItem);
+        });
+    }
+
+    itemCheckChanged(item: ListItem){
+        if (!item.hadFirstUpdate){
+            item.hadFirstUpdate = true;
+            return;
+        }
+        item.complete = !item.complete;
+        this.database.execSQL("UPDATE ListItem SET Complete = ?, ModifiedUTC = ? WHERE Id = ?", 
+            [item.complete ? 1 : 0, (new Date()).getUTCMilliseconds(), item.id])
+            .then(id => {},error => {console.log("UPDATE DB ERROR", error)});
     }
 }
--- app/listItem.ts
+++ app/listItem.ts
@@ -1,5 +1,8 @@
 export class ListItem {
-    constructor(public description:string, public complete:boolean = false){
-        
+    //This checkbox control is annoying; when it first renders it apparently gets "updated", and we don't want to 
+    //do anything with this fake update.
+    hadFirstUpdate: boolean = false;
+
+    constructor(public id:string, public description:string, public complete:boolean = false){
     }
 }
