07cf4ec9ec87d460b994e4ed3fe63e00b4a44293
Daniel Elkington <danielelkington@me.com>
2017-07-20 14:01:26 +1000

    Adding backend project

---
+++ backend/ListItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace backend
+{
+    public class ListItem
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Complete { get; set; }
+        public int ModifiedUTC { get; set; }
+    }
+}
---
+++ backend/ListItemController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace backend
+{
+    public class ListItemController : ApiController
+    {
+        public IEnumerable<ListItem> Post(int lastSyncTime, IEnumerable<ListItem> items)
+        {
+            return items;
+            //return new List<ListItem> { new ListItem { Id = "124" } };
+        }
+    }
+}
